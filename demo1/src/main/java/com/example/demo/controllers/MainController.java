0b98cb849948666c1eecf386355ebf9be839a268
Назар <105419931+Naza1re@users.noreply.github.com>
2023-03-23 01:52:32 +0300

    Add files via upload

---
+++ demo1/src/main/java/com/example/demo/controllers/MainController.java
@@ -0,0 +1,29 @@
+package com.example.demo.controllers;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class MainController {
+
+    @Value("${spring.application.name}")
+    String appName;
+
+    @GetMapping("/")
+    public String homePage(Model model) {
+        model.addAttribute("title", "Главная страница");
+        return "home";
+    }
+
+    @GetMapping("/about")
+    public String about(Model model) {
+        model.addAttribute("title", "Про нас");
+        return "about";
+    }
+
+
+
+
+}
